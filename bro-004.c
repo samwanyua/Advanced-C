a0af6f85a011ccd2a002094bc5453b6fb8d0a51f
samwanyua <samexample8@gmail.com>
2023-11-14 11:35:08 +0300

    Functions

---
+++ bro-004.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+void birthday()
+{
+   printf("\nHappy birthday to you!");
+   printf("\nHappy birthday to you!");
+   printf("\nHappy birthday dear ...");
+   printf("\nHappy birthday to you!\n\n");
+}
+
+int main(void)
+{
+// function is a small section of code that is executed when we call it #invoking a function
+    birthday();
+    birthday();
+    birthday();
+    birthday();
+
+
+    return 0;
+}
