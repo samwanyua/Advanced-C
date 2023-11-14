e72f96342a00479ae03df0ffd84e58c5eef449bb
samwanyua <samexample8@gmail.com>
2023-11-14 11:20:42 +0300

    OR || logical opertor

---
+++ bro-002.c
@@ -0,0 +1,25 @@
+/*|| or logical operator*/
+#include <stdio.h>
+
+int main(void)
+{
+
+// || (or) checks if at least one condition is true
+
+float temp = 40;
+
+if(temp <= 0 || temp >=30 )
+{
+    printf("\nThe weather is bad!\n");
+}
+
+else 
+{
+    printf("\n The weather is good!\n");
+}
+
+
+
+    return 0;
+}
+
