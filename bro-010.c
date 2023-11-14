ff9897a73f2b459587a98c778e4affe4556ad720
samwanyua <samexample8@gmail.com>
2023-11-14 18:01:19 +0300

    For loop

---
+++ bro-010.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+
+//For loop = repeats a section of a code for a limited amount of times
+
+    for (int i= 10; i >=1; i--) // i is the short form for index. 3 statements: counter or index( i = 1;), condition(i <= 10;), increment/decrement (i++)
+    {
+        printf("%d\n",i);
+    }
+
+    return 0;
+}
